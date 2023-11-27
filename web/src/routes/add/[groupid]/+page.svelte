234c88d242dae647991c7e3dafb60b4ca5ed0c95
Narfinger <Narfinger@users.noreply.github.com>
2023-11-27 20:35:56 +0900

    some work

--- web/src/routes/add/[groupid]/+page.svelte
+++ web/src/routes/add/[groupid]/+page.svelte
@@ -1,30 +1,46 @@
 <script>
-	import { enhance } from '$app/forms';
+	import { superForm, superValidateSync } from 'sveltekit-superforms/client';
+	import { redirect } from '@sveltejs/kit';
+	import { z } from 'zod';
+	const schema = z.object({
+		description: z.string().default('Hello world!'),
+		total: z.number().positive(),
+		select: z.string().default('0')
+	});
+	const { form, errors, enhance } = superForm(superValidateSync(schema), {
+		SPA: true,
+		validators: schema,
+		onUpdate({ form }) {
+			if (form.valid) {
+				// TODO: Do something with the validated form.data
+				console.log('redirecting');
+				throw redirect(303, '/');
+			} else {
+				console.log('INVALID FORM');
+				console.log(form.errors);
+			}
+		}
+	});
 	let data = {
 		people: ['Christian', 'Emilia']
-	};
-
-	export const actions = {
-		default: async ({ cookies, request }) => {
-			const data = await request.formData();
-		}
 	};
 </script>
 
 <h1>ADDD</h1>
 <h2>Adding for {data['people']}</h2>
-<form method="POST">
+<form method="POST" action="default" use:enhance>
 	<label>
-		Expense: <input name="description" autocomplete="off" />
+		Expense: <input name="description" autocomplete="off" bind:value={$form.description} />
 	</label>
 	<label>
-		Total: <input name="total" autocomplete="off" />
+		Total: <input name="total" autocomplete="off" type="number" bind:value={$form.total} />
 	</label>
 	<label>
 		Paye:
-		<select name="pay_type" id="cars">
-			<option value="volvo">{data['people'][0]} is paying 50/50</option>
-			<option value="saab">{data['people'][1]} is paying 50/50</option>
+		<select name="pay_type" id="cars" bind:value={$form.select}>
+			<option value="0">{data['people'][0]} is paying 50/50</option>
+			<option value="1">{data['people'][1]} is paying 50/50</option>
 		</select>
 	</label>
+	<button type="submit">Submit</button>
 </form>
