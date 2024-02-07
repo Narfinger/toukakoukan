0948cd6430f9154282dfdb241dbfbb29ea3a6c0a
Narfinger <Narfinger@users.noreply.github.com>
2024-02-07 15:05:42 +0900

    basic pwa

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -4,6 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({}), svelte()
+  plugins: [VitePWA({
+    registerType: 'autoUpdate', workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    }
+  }),
+  svelte()
   ]
 })
