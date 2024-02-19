419e53bf7cab879d6cf6e7e8b9b60e1ce7c2782c
Narfinger <Narfinger@users.noreply.github.com>
2024-02-19 18:49:44 +0900

    make offline work

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -6,24 +6,39 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [VitePWA({
+    manifest: {
+      icons: [
+        {
+          src: "/512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable"
+        }
+      ]
+    },
     devOptions: {
       enabled: true
     },
     workbox: {
       globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-
-      runtimeCaching: [{
-        urlPattern: "http://localhost:.*\\/api\\/.*",
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "api-cache",
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }]
-    },
-    registerType: 'autoUpdate',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url, sameOrigin }) => url.pathname.match(/^\/api/),
+          handler: 'StaleWhileRevalidate',
+          method: 'GET',
+          options: {
+            cacheName: 'api-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    }
   }),
   svelte()
   ]
