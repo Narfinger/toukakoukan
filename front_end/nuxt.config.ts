e353a99eff6fc9b5631179a5aa46760b6a835609
Narfinger <Narfinger@users.noreply.github.com>
2025-01-23 15:23:29 +0900

    initial

---
+++ front_end/nuxt.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [ '@vite-pwa/nuxt'],
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {    plugins: [      tailwindcss(),    ],  },
+
+})
