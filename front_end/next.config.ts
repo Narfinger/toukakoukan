935dd3cb51907b19d15e64ffc42ff8aea0481e63
Narfinger <Narfinger@users.noreply.github.com>
2025-01-23 15:57:29 +0900

    some stuff

--- front_end/next.config.ts
+++ front_end/next.config.ts
@@ -4,4 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+      },
+    ]
+  },
+}
+
 export default nextConfig;
