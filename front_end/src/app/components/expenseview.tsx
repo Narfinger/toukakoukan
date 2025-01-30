7d39d4318390fd2718a251ac237da9a283c8dba4
Narfinger <Narfinger@users.noreply.github.com>
2025-01-30 15:08:23 +0100

    some stuff

--- front_end/src/app/components/expenseview.tsx
+++ front_end/src/app/components/expenseview.tsx
@@ -10,21 +10,32 @@ function expensemap(expense: Expense) {
     )
 }
 
-function groupmap(g: Group) {
-    return <li>
-        {g.name}
-    </li>;
+function groupmap(g: Group, active: number, setActive) {
+    if (active == g.id) {
+        return <a
+            role="tab"
+            className="tab tab-active text-primary [--tab-bg:yellow] [--tab-border-color:orange]"
+            onClick={setActive(g.id)}
+        >
+            {g.name}
+        </a>
+    } else {
+        <a role="tab" className="tab text-primary [--tab-bg:yellow] [--tab-border-color:orange]" onClick={setActive(g.id)}>{g.name}</a>
+    }
 }
 
 export default function ExpenseView({ user_id }: { user_id: number }) {
     const [groups, setGroups] = useState<Group[]>([])
+    const [active, setActive] = useState<number>(0);
 
     const expenses: Expense[] = [];
 
     useEffect(() => {
         async function fetchGroups() {
-            const res = await fetch('/groups')
-            const data = await res.json()
+            const res = await fetch('/api/groups/')
+            console.log(res);
+            const data = await res.json();
+            console.log(data);
             setGroups(data)
         }
         fetchGroups()
@@ -32,13 +43,18 @@ export default function ExpenseView({ user_id }: { user_id: number }) {
 
 
     const exp = expenses.map((e: Expense[]) => expensemap(e));
-    const groups_elem = groups.map((g: Group) => groupmap(g));
+    const groups_elem = groups.map((g: Group) => groupmap(g, active, () => setActive));
     return (<>
         <div className="row">
-            <ul>
+            <div>
+                {"here is the tab list"}
+            </div>
+
+            <div role="tablist" className="tabs tabs-lifted">
                 {groups_elem}
-            </ul>
+            </div>
         </div>
+
         <div className="row">
             <table className="table">
                 <thead>
--- front_end/src/app/components/userview.tsx
+++ front_end/src/app/components/userview.tsx
@@ -1,5 +1,5 @@
 export default function LoginView({ user_id }: { user_id: number }) {
 
-    return <span>{"Here is the user information"}</span>
+    return <span>{"Here is stuff"}{user_id}</span>
 
 }
